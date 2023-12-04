c38bace18c3a24a3ccb93071c09e4279869dcb4c
ElmazMed <elmazgour.mohamed@gmail.com>
2023-12-04 01:22:22 +0100

    remove and edit buttons

--- script.js
+++ script.js
@@ -8,17 +8,52 @@ addNewBtn.addEventListener('click', () => {
     
 })
 
-let tasks = [
-    
-]
-
 let taskInput = document.getElementById('task-inpt')
 let taskDescription = document.getElementById('description-inpt')
 let taskDate = new Date()
-console.log(taskDate);
 let fullDate = taskDate.getDate() + "/" + (taskDate.getMonth() +1) + "/" + taskDate.getFullYear()
+let tasks = []
+
+function addTasksToPage() {
+    document.getElementsByClassName("tasks-table")[0].innerHTML = ""
+    for(let task of tasks) {
+        let tasksContent = 
+        `
+            <div class="task | d-flex space-between items-center">
+                <div class="task-texts">
+                    <h3 class="title">${task.title}</h3>
+                    <p class="desc">${task.description}</p>
+                    <div class="d-flex">
+                        <span class="material-symbols-outlined">calendar_month</span>
+                        <span class="date">${task.date}</span>
+                    </div>
+                </div>
+                
+                <div class="task-cta | d-flex">
+                    <span class="material-symbols-outlined | edit">
+                        edit
+                    </span>
+                    <span class="material-symbols-outlined | done">
+                        done
+                    </span>
+                    <span class="material-symbols-outlined | remove">
+                        delete
+                    </span>
+                </div>
+            </div>
+        `
+        document.getElementsByClassName("tasks-table")[0].innerHTML += tasksContent
+        removeTask()
+        editTask()
+    }
+}
+addTasksToPage()
 
 document.getElementById("add-task-btn").addEventListener('click', ()=> {
+    if(taskInput.value == "" && taskDescription.value == "") {
+        alert("Please insert a task")
+        return
+    }
 
     let newTask = {
         title: taskInput.value,
@@ -27,50 +62,38 @@ document.getElementById("add-task-btn").addEventListener('click', ()=> {
         isDone: false,
     }
     tasks.push(newTask)
+    addTasksToPage()
+    taskDescription.value = ""
+    taskInput.value = ""
 
-    if(taskInput.value == "" && taskDescription.value == "") {
-        alert("Please insert a task")
-        return
-    }
-
-    document.getElementsByClassName('tasks-table')[0].innerHTML += `  
-    <div class="task | d-flex space-between items-center">
-        <div class="task-texts">
-            <h3 class="title">${newTask.title}</h3>
-            <p class="desc">${newTask.description}</p>
-            <div class="d-flex">
-                <span class="material-symbols-outlined">calendar_month</span>
-                <span class="date">${newTask.date}</span>
-            </div>
-        </div>
-        <div class="task-cta | d-flex">
-            <span id="edit" class="material-symbols-outlined">
-                edit
-            </span>
-            <span class="material-symbols-outlined | done">
-                done
-            </span>
-            <span id="remove" class="material-symbols-outlined">
-                delete
-            </span>
-        </div>
-    </div>
-`
-taskDescription.value = ""
-taskInput.value = ""
-doneBtn()
 })
 
-function doneBtn(){
-    let doneBtns = document.getElementsByClassName("done")
+function removeTask() {
+    let removeBtns = document.getElementsByClassName("remove")
+            for(let i = 0; i < removeBtns.length; i++) {
+                removeBtns[i].addEventListener("click", ()=>{
 
-for(let doneBtn of doneBtns) {
-    doneBtn.addEventListener("click", ()=> {
-       let tsaksContainer = document.getElementsByClassName("task")
-
-       for(let tsakContainer of tsaksContainer) {
-        tsakContainer.style.backgroundColor = "#dadada"
-       }
-    })
+                let confrimDelete = confirm("I confrim to delete the task!")
+                if(confrimDelete) {
+                    tasks.splice(tasks[i], 1)
+                    taskDescription.value = ""
+                    taskInput.value = ""
+                    addTasksToPage()
+                }
+            })
+        }
 }
+
+function editTask() {
+    let editBtns = document.getElementsByClassName("edit")
+
+    for (let index = 0; index < editBtns.length; index++) {
+        let editBtn = editBtns[index];
+
+        editBtn.addEventListener("click", ()=> {
+           taskInput.value = tasks[index].title;
+           taskDescription.value = tasks[index].description;
+        })
+        
+    }
 }
